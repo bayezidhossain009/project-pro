69c77892702b0ad480f03c34821dd4e11a9d7cb0
bayezidhossain009 <bayezidhossain058@gmail.com>
2026-07-05 23:18:12 +0600

    Add Capacitor Android

---
+++ android/app/src/main/java/com/machinetracker/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.machinetracker.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.machinetracker.app',
+  appName: 'Machine Tracker',
+  webDir: 'dist'
+};
+
+export default config;
